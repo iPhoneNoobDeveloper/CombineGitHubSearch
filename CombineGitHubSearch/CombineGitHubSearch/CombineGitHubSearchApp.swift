e46e838b399d33066481f61b65267dd0b29bc35c
Nirav Jain <niravjain02@gmail.com>
2025-09-28 14:38:40 -0700

    Initial commit

---
+++ CombineGitHubSearch/CombineGitHubSearch/CombineGitHubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineGitHubSearchApp.swift
+//  CombineGitHubSearch
+//
+//  Created by Nirav Jain on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineGitHubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
